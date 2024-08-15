e2d51f6777066c74cc0766ea82c547e43b32c24a
SeanNG21 <nguyenviethung21092003@gmail.com>
2024-08-15 10:00:35 +0700

    fix

--- envsetup.sh
+++ envsetup.sh
@@ -1,27 +1,39 @@
 #!/bin/bash
-cd app
-if [ -d "env" ]
-then
-    echo "Python virtual env exixts"
+
+# Navigate to the app directory
+cd app || { echo "App directory not found!"; exit 1; }
+
+# Check if the Python virtual environment exists
+if [ -d "env" ]; then
+    echo "Python virtual env exists"
 else
     python3.11 -m venv env
+    if [ $? -ne 0 ]; then
+        echo "Failed to create virtual environment. Exiting."
+        exit 1
+    fi
 fi
 
-echo $PWD
-source env/bin/activate
+# Ensure the virtual environment directory exists before activation
+if [ -f "env/bin/activate" ]; then
+    source env/bin/activate
+else
+    echo "Virtual environment activation script not found!"
+    exit 1
+fi
 
+# Install dependencies from requirements.txt
 pip install -r requirements.txt
 
-
-
-if [ -d "logs" ]
-then
-    echo "log folder exixts"
+# Check if the logs directory exists
+if [ -d "logs" ]; then
+    echo "Log folder exists"
 else
     mkdir logs
     touch logs/error.log logs/access.log
 fi
 
-sudo chmod -R 777 logs
+# Set permissions for the logs directory
+chmod -R 777 logs
 
 echo "envsetup finishes"
